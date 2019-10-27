7010a918f9463c1bb56589fb371d2c2057403d35
Adam Deeley <adam.deeley@oneiota.co.uk>
2019-10-27 21:27:33 +0000

    Refactor initNodeGrid.

--- enums/colours.enum.ts
+++ enums/colours.enum.ts
@@ -1,3 +1,5 @@
 export enum ColoursEnum {
 	grey = '#37474F',
+	black = '#000000',
+	white = '#ffffff',
 }
--- models/nodes.model.ts
+++ models/nodes.model.ts
@@ -1,73 +1,84 @@
 import { Node } from "./node.model";
 import { RectModel } from "./rect.model";
+import { ColoursEnum } from "../enums/colours.enum";
 
 export class Nodes {
-	private x = 2;
-	private y = 2;
 	private gridSz: number;
 	private nodeSz = 20;
 	private grid = [];
+	private borderSize = 2;
 
 	constructor(private ctx, private canvas, public n: number) {
-		this.gridSz = n
+		this.gridSz = n;
+		this.initNodeGrid();
+	}
 
-		//Initialise the 2d nodes array
-		for (var i = 0; i < this.gridSz; i++) {
-			var row = [];
-			for (var j = 0; j < this.gridSz; j++) {
-				const rectModel = new RectModel(this.x, this.y, this.nodeSz, this.nodeSz);
-				row.push(new Node(ctx, rectModel, i, j));
-				this.x += this.nodeSz + 2;
-			}
+	private initNodeGrid() {
+		let x = this.borderSize;
+		let y = this.borderSize;
+
+		for (let i = 0; i < this.gridSz; i++) {
+			const row = this.fillRow(x, y, i);
 			this.grid.push(row);
-			this.x = 2;
-			this.y += this.nodeSz + 2;
+			x = this.borderSize;
+			y += (this.nodeSz + this.borderSize);
 		}
 	}
 
-	public drawStartAndEndNodes() {
+	private fillRow(x, y, i): Array<Node> {
+		const row = [];
+		for (let j = 0; j < this.gridSz; j++) {
+			const rectModel = new RectModel(x, y, this.nodeSz, this.nodeSz);
+			row.push(new Node(this.ctx, rectModel, i, j));
+			x += (this.nodeSz + this.borderSize);
+		}
+		return row;
+	}
+
+	public drawStartAndEndNodes(): void {
 		// Start node
 		this.ctx.beginPath();
 		this.ctx.arc(2, 2, this.nodeSz, 0, 2 * Math.PI);
-		this.ctx.fillStyle = "#ffffff";
+		this.ctx.fillStyle = ColoursEnum.white;
 		this.ctx.fill();
 		this.ctx.closePath();
 
 		this.ctx.font = "12pt serif"
-		this.ctx.fillStyle = "#000000";
+		this.ctx.fillStyle = ColoursEnum.black;
 		this.ctx.fillText("S", 3, 12);
 
 		// End node
 		this.ctx.beginPath();
-		this.ctx.arc(this.canvas.width - 2, this.canvas.height - 2, this.nodeSz, 0, 2 * Math.PI);
-		this.ctx.fillStyle = "#ffffff";
+		this.ctx.arc(
+			this.canvas.width - this.borderSize,
+			this.canvas.height - this.borderSize,
+			this.nodeSz, 0,
+			2 * Math.PI
+		);
+		this.ctx.fillStyle = ColoursEnum.white;
 		this.ctx.fill();
 		this.ctx.closePath();
 
 		this.ctx.font = "12pt serif"
-		this.ctx.fillStyle = "#000000";
+		this.ctx.fillStyle = ColoursEnum.black;
 		this.ctx.fillText("F", this.canvas.width - 12, this.canvas.height - 3);
 	}
 
 
-	public drawAll() {
-		for (var i = 0; i < this.gridSz; i++) {
-			for (var j = 0; j < this.gridSz; j++) {
+	public drawAll(): void {
+		for (let i = 0; i < this.gridSz; i++) {
+			for (let j = 0; j < this.gridSz; j++) {
 				this.grid[i][j].draw();
 			}
 		}
 		this.drawStartAndEndNodes();
 	}
 
-	public getNode(row, col) {
-		/**
-		 * returns the node at row, col
-		 * @row, col : ints
-		 */
+	public getNode(row: number, col: number): Node {
 		return this.grid[row][col];
 	}
 
-	public joinNodes(nodeA, nodeB) {
+	public joinNodes(nodeA, nodeB): void {
 		/**
 		 * Joins the nodes a and b together visually by merging the two boxes
 		 * a and b on the canvas.
@@ -76,22 +87,22 @@ export class Nodes {
 		 * below, left or right.
 		 */
 		if (nodeA.x < nodeB.x) {
-			nodeA.w += 2;
+			nodeA.w += this.borderSize;
 		}
 		else if (nodeB.x < nodeA.x) {
-			nodeB.w += 2;
+			nodeB.w += this.borderSize;
 		}
 		else if (nodeA.y < nodeB.y) {
-			nodeA.h += 2;
+			nodeA.h += this.borderSize;
 		}
 		else if (nodeB.y < nodeA.y) {
-			nodeB.h += 2;
+			nodeB.h += this.borderSize;
 		}
 		nodeB.openUp();
 	}
 
-	public getNeighbours(row: number, col: number): Array<any> {
-		var neighbours = [];
+	public getNeighbours(row: number, col: number): Array<Node> {
+		const neighbours = [];
 
 		//left of node
 		if (row > 0) {
